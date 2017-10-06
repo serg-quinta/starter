e1f30b988fcec06e652480bd7a769cabab4d32d5
Serhiy Valchuk <svalchuk@gmail.com>
2017-10-06 13:46:22 +0300

    moved templates to nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,67 @@
 'use strict';
 
-const gulp = require('gulp');
-const less = require('gulp-less');
-const sourcemaps = require('gulp-sourcemaps');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-// const pug = require('gulp-pug');
-const nunjucksRender = require('gulp-nunjucks-render');
+const gulp            = require('gulp');
+const less            = require('gulp-less');
+const sourcemaps      = require('gulp-sourcemaps');
+const postcss         = require('gulp-postcss');
+const autoprefixer    = require('autoprefixer');
+const data            = require('gulp-data');
+const nunjucksRender  = require('gulp-nunjucks-render');
+const prettify        = require('gulp-prettify');
+const browserSync     = require('browser-sync').create();
+const babel           = require('gulp-babel');
+const concat          = require('gulp-concat');
 
 // Styles
-gulp.task('styles', function() {
-  return gulp.src('./src/less/main.less')
-      .pipe(sourcemaps.init())
-      .pipe(less())
-      .pipe(postcss([ autoprefixer({ browsers: ['> 0.5%', 'last 2 version', 'Firefox ESR'] }) ]))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('css'));
+gulp.task('styles', function () {
+  return gulp.src('src/less/main.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(postcss([autoprefixer({ browsers: ['> 0.5%', 'last 2 version', 'Firefox ESR'] })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/css'));
 });
 
 // HTML
-// gulp.task('pug', function(){
-//   return gulp.src('./src/*.pug')
-//     .pipe(pug({pretty: true}))
-//     .pipe(gulp.dest('./'))
-// });
-gulp.task('html', function(){
+gulp.task('html', function () {
   return gulp.src('src/templates/**/[^_]*.html')
     .pipe(nunjucksRender({
-      path: ['src/templates/'] // String or Array 
+      path: ['src/templates/']
     }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('public'));
+});
+
+// 
+gulp.task('js', () => {
+    return gulp.src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('public/js'));
 });
 
 // Build
-gulp.task('build', gulp.parallel('styles', 'html'));
+gulp.task('build', gulp.parallel('styles', 'html', 'js'));
 
 // Watch
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('./src/less/**/*.less', gulp.series('styles'));
-  gulp.watch('./src/**/*.html', gulp.series('html'));
+  gulp.watch('./src/templates/**/*.html', gulp.series('html'));
+  gulp.watch('./src/js/**/*.js', gulp.series('js'));
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: 'public'
+  });
+
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
 });
 
 // Dev
-gulp.task('dev', gulp.series('build', 'watch'));
+gulp.task('dev',
+  gulp.series('build', gulp.parallel('watch', 'serve'))
+);
